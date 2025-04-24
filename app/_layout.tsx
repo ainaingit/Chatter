af4151e8f6f2fc8c517ae97876bd25da3d0241a5
RAMAROSON Aina Kevin <ainakevinramaroson@gmail.com>
2025-04-24 10:44:54 +0300

    update structure

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,9 +3,6 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="signin" options={{ title: "Sign In", headerShown: false }} />
-      <Stack.Screen name="signup" options={{ title: "Sign Up", headerShown: false }} />
-      
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       <Stack.Screen name="+not-found" />
     </Stack>
