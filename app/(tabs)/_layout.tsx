4da588cd9189224ce2fc12be8cc677e186e5a787
RAMAROSON Aina Kevin <ainakevinramaroson@gmail.com>
2025-04-30 14:00:00 +0300

    changed tabs icons
    -used Ionicons for two tabs
    - used Fontawesome for profil icons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,47 +1,54 @@
 import React from "react";
 import { SafeAreaView } from "react-native";
-import { Tabs } from "expo-router"; // Assuming Tabs is from expo-router
-import { Title } from "react-native-paper";
-import { Ionicons } from '@expo/vector-icons'; // Import des icônes modernes (Ionicons)
+import { Tabs } from "expo-router"; // Pour les onglets
+import { Title } from "react-native-paper"; // Pour le titre de l'en-tête
+import { FontAwesome, Ionicons } from '@expo/vector-icons'; // Icônes modernes
 
 export default function TabLogin() {
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <Tabs>
+        {/* Onglet Accueil */}
         <Tabs.Screen
           name="index"
           options={{
-            headerShown: false,title : 'ome',
-            tabBarIcon: () => (
-              <Ionicons name="chatbubble-ellipses-outline" size={24} color="black" /> // Icône Chat (style WhatsApp)
+            title: "Accueil",
+            tabBarIcon: ({ color, size }) => (
+              <FontAwesome name="home" size={size} color={color} />
             ),
           }}
         />
+
+        {/* Onglet Status */}
         <Tabs.Screen
           name="update"
           options={{
             headerTitle: () => <Title style={{ fontSize: 20 }}>Mettre à jour le statut</Title>,
             headerShown: false,
-            tabBarIcon: () => (
-              <Ionicons name="sync-outline" size={24} color="black" /> // Icône Update (style WhatsApp)
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="sync-outline" size={size} color={color} /> // Icône de mise à jour
             ),
           }}
         />
+
+        {/* Onglet Paramètres */}
         <Tabs.Screen
           name="settings"
           options={{
             headerShown: false,
-            tabBarIcon: () => (
-              <Ionicons name="person-outline" size={24} color="black" /> // Icône Settings (style WhatsApp)
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="settings-outline" size={size} color={color} /> // Icône des paramètres
             ),
           }}
         />
+
+        {/* Onglet Profil */}
         <Tabs.Screen
           name="profil"
           options={{
             headerShown: false,
-            tabBarIcon: () => (
-              <Ionicons name="settings-outline" size={24} color="black" /> // Icône Settings (style WhatsApp)
+            tabBarIcon: ({ color, size }) => (
+              <FontAwesome name="user" size={size} color={color} /> // Icône du profil
             ),
           }}
         />
